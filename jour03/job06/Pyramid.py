68bb0df3b98cf00eae1d14b411aae9bb59b7b0da
pelayo-joel <joel.pelayo@laplateforme.io>
2023-01-05 11:05:30 +0100

    [UPDATE] Changed Pyramid.py to match the assignement

--- jour03/job06/Pyramid.py
+++ jour03/job06/Pyramid.py
@@ -1,8 +1,19 @@
-def AlphabeticalPyramid():
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
+def AlphabeticalPyramid(alphabet):
     n = 0
     while n <= len(alphabet):
         print(alphabet[:n])
         n += 1
+        alphabet = alphabet[n-1:]
     
-AlphabeticalPyramid()
+alphabetPy = "abcdefghijklmnopqrstuvwxyz"
+AlphabeticalPyramid(alphabetPy * 10)
+
+
+#######---------Previous version---------#######
+
+#def AlphabeticalPyramid():
+#    alphabet = "abcdefghijklmnopqrstuvwxyz"
+#    n = 0
+#    while n <= len(alphabet):
+#        print(alphabet[:n])
+#        n += 1
